44bab7c1cc145ca41210b5f8806a2d65396c7c40
TzuHanLiang <crocodilestear@gmail.com>
2022-03-08 18:06:09 +0800

    feat: add menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Header = (props) => {
-  return <div className="header"></div>;
+  return (
+    <div className={`header${props.menuOpen ? " active" : ""}`}>
+      <div className="header__logo">
+        <img className="header__logo--image" src="/LOGO.png" alt="CAFECA" />
+      </div>
+      <button className="header__hambuger" onClick={props.toggleMenuHandler}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+    </div>
+  );
 };
 
 export default Header;
