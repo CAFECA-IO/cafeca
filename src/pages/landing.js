419d709ecf9e1a48846111234406b1a816da58d1
TzuHanLiang <crocodilestear@gmail.com>
2022-03-09 15:07:32 +0800

    feat: #4 add card

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Features from "../components/Features";
 import Main from "../components/Main";
+import Specialize from "../components/Specialize";
 
 const Landing = (props) => {
-  return <div className="landing">
-    <Main/>
-    <Features/>
-  </div>;
+  return (
+    <div className="landing">
+      <Main />
+      <Features />
+      <Specialize />
+    </div>
+  );
 };
 
 export default Landing;
