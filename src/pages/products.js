ea71a2c4f93b4fc7450e260c0b4e03a25038ca69
TzuHanLiang <crocodilestear@gmail.com>
2022-03-21 16:58:59 +0800

    feat: add product animation 2 size

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,18 +1,69 @@
-import React from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 
 const Products = (props) => {
   const { t } = useTranslation();
+  const [currentContent, setCurrentContent] = React.useState("intro");
+  const introRef = useRef();
+  const fingerRef = useRef();
+  const offlineRef = useRef();
+  const deviceRef = useRef();
+  const traceRef = useRef();
+
+  const handleWindowScroll = useCallback(() => {
+    var windowHeight = window.innerHeight;
+    var introTop = introRef.current.getBoundingClientRect().top;
+    console.log(
+      `introTop`,
+      introTop,
+      introTop < windowHeight - introRef.current.clientHeight
+    );
+
+    var fingerTop = fingerRef.current.getBoundingClientRect().top;
+    console.log(`fingerTop`, fingerTop);
+
+    var offlineTop = offlineRef.current.getBoundingClientRect().top;
+    console.log(`offlineTop`, offlineTop);
+
+    var deviceTop = deviceRef.current.getBoundingClientRect().top;
+    console.log(`deviceTop`, deviceTop);
+
+    var traceTop = traceRef.current.getBoundingClientRect().top;
+    console.log(`traceTop`, traceTop);
+
+    // var elementVisible = 150;
+    if (introTop < windowHeight - introRef.current.clientHeight) {
+      setCurrentContent((prev) => (prev !== "intro" ? "finger-back" : "intro"));
+    }
+    if (fingerTop < windowHeight - fingerRef.current.clientHeight) {
+       setCurrentContent("finger");
+    }
+    if (offlineTop < windowHeight - offlineRef.current.clientHeight) {
+       setCurrentContent("offline");
+    }
+    if (deviceTop < windowHeight - deviceRef.current.clientHeight) {
+       setCurrentContent("device");
+    }
+    if (traceTop < windowHeight - traceRef.current.clientHeight) {
+       setCurrentContent("trace");
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleWindowScroll);
+    return () => window.removeEventListener("scroll", handleWindowScroll);
+  }, [handleWindowScroll]);
+
   return (
-    <div className="products">
+    <div className={`products ${currentContent}`}>
       <div className="products__container">
-        <div className="products__intro">
+        <div className="products__intro" ref={introRef}>
           <div className="products__intro-image">
             <div className="products__intro-image products__intro-image--circle"></div>
             <div className="products__intro-image products__intro-image--card">
-              <div class="products__intro-image--card-inner">
-                <div class="products__intro-image--card-front"></div>
-                <div class="products__intro-image--card-back"></div>
+              <div className="products__intro-image--card-inner">
+                <div className="products__intro-image--card-front"></div>
+                <div className="products__intro-image--card-back"></div>
               </div>
             </div>
           </div>
@@ -21,10 +72,12 @@ const Products = (props) => {
             <p className="products__intro-description header-secondary">
               {t("product_introducion")}
             </p>
-            <button className="products__intro-button button buttom--icon"></button>
+            <a href="/products/#finger">
+              <button className="products__intro-button button buttom--icon"></button>
+            </a>
           </div>
         </div>
-        <div className="products__finger">
+        <div className="products__finger" id="finger" ref={fingerRef}>
           <div className="products__image-box">
             <div className="products__image products__finger-image--background"></div>
             <div className="products__image products__finger-image--card"></div>
@@ -43,7 +96,7 @@ const Products = (props) => {
           </div>
         </div>
         <div className="products__line-image products__line-image--1"></div>
-        <div className="products__offline">
+        <div className="products__offline" ref={offlineRef}>
           <div className="products__image-box">
             <div className="products__image products__offline-image--cloud"></div>
             <div className="products__image products__offline-image--card"></div>
@@ -62,7 +115,7 @@ const Products = (props) => {
           </div>
         </div>
         <div className="products__line-image products__line-image--2"></div>
-        <div className="products__device">
+        <div className="products__device" ref={deviceRef}>
           <div className="products__image-box">
             <div className="products__image products__device-image--devices"></div>
             <div className="products__image products__device-image--card"></div>
@@ -81,7 +134,7 @@ const Products = (props) => {
           </div>
         </div>
         <div className="products__line-image products__line-image--3"></div>
-        <div className="products__trace">
+        <div className="products__trace" ref={traceRef}>
           <div className="products__image-box">
             <div className="products__image products__trace-image--spotlight"></div>
             <div className="products__image products__trace-image--card"></div>
