7efbd757d2661bba5e32c53b4d30ca205e584ea6
TzuHanLiang <crocodilestear@gmail.com>
2022-03-25 15:00:17 +0800

    feat: #59

--- src/components/Data.js
+++ src/components/Data.js
@@ -23,26 +23,60 @@ const Data = (props) => {
   const countryRef = useRef();
   const CAGRRef = useRef();
   const userRef = useRef();
+
+  // const [observerObj, setObserverObj] = React.useState(null);
   // const containerEl = document.querySelector(".data__container");
   // const countryEl = document.querySelector(".data__count--country");
   // const CAGREl = document.querySelector(".data__count--CAGR");
   // const userEl = document.querySelector(".data__count--user");
 
   const handleWindowScroll = useCallback(() => {
-    var windowHeight = window.innerHeight;
-    var elementTop = containerRef.current.getBoundingClientRect().top;
-    var elementVisible = containerRef.current.clientHeight;
-    if (elementTop < windowHeight - elementVisible) {
-      // setTriggered(true);
-      animateValue(countryRef.current, 0, 70, 500);
-      animateValue(CAGRRef.current, 0, 54.8, 800, true);
-      animateValue(userRef.current, 0, 6300, 1000);
+    let windowHeight = window.innerHeight;
+    let elementTop = containerRef.current.getBoundingClientRect().top;
+    let elementVisible = containerRef.current.clientHeight;
+    // if (!triggered) {
+    if (
+      elementTop < windowHeight - elementVisible / 2 &&
+      elementTop > -elementVisible / 2
+    ) {
+      setTriggered(true);
+      if (!triggered) {
+        animateValue(countryRef.current, 0, 70, 500);
+        animateValue(CAGRRef.current, 0, 54.8, 800, true);
+        animateValue(userRef.current, 0, 6300, 1000);
+      }
     } else {
-      // setTriggered(false);
+      setTriggered(false);
     }
-  }, [countryRef, CAGRRef, userRef]);
+    // }
+  }, [triggered, countryRef, CAGRRef, userRef]);
+
+  // const addObserver = (element, options) => {
+  //   let io_observer = new IntersectionObserver((entries, observer) => {
+  //     const ratio = entries[0].intersectionRatio;
+  //     const boundingRect = entries[0].boundingClientRect;
+  //     const intersectionRect = entries[0].intersectionRect;
+  //     if (ratio === 0) {
+  //       console.log("outside");
+  //     } else if (ratio < 1) {
+  //       if (boundingRect.top < intersectionRect.top) {
+  //         console.log("on the top");
+  //       } else {
+  //         console.log("on the bottom");
+  //       }
+  //     } else {
+  //       console.log("inside");
+  //     }
+  //     setObserverObj({
+  //       observer,
+  //       entries,
+  //     });
+  //   }, options);
+  //   io_observer.observe(element);
+  // };
 
   useEffect(() => {
+    // addObserver(countryRef?.current)
     window.addEventListener("scroll", handleWindowScroll);
     return () => window.removeEventListener("scroll", handleWindowScroll);
   }, [handleWindowScroll]);
